592dc5f02c41d1d31efe07aa801a6f965f7b34d9
GeorgiaTsoukala <georgiats1999@gmail.com>
2024-03-15 17:37:38 +0100

    login

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,14 +1,104 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
 
 const LoginScreen = () => {
+  const[email, setEmail] = useState('')
+  const[password, setPassword] = useState('')
+
   return (
-    <View>
-      <Text>Login Screen!afdgshdf</Text>
-    </View>
+    <KeyboardAvoidingView 
+      style={styles.container}
+      behavior="padding"
+      >
+      <View style={styles.inputContainer}> 
+        <TextInput 
+          placeholder="Email"
+          value={email}
+          onChangeText={text => setEmail(text)}
+          style={styles.input}
+          >
+        </TextInput>
+        <TextInput 
+          placeholder="Password"
+          value={password}
+          onChangeText={text => setPassword(text)}
+          style={styles.input}
+          secureTextEntry
+          >
+        </TextInput>
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+         onPress={() => {}}
+         style={styles.button}
+        >
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+         onPress={() => {}}
+         style={[styles.button, styles.buttonOutline]}
+        >
+          <Text style={styles.buttonOutlineText}>Register</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   )
 }
 
 export default LoginScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    //backgroundColor: "#267777",
+  },
+  // boxContainer: {
+  //   justifyContent: "center",
+  //   width: "80%",
+  //   height: 480,
+  //   borderRadius: 10,
+  //   alignItems: "center",
+  //   backgroundColor: "#efefef",
+  // },
+  inputContainer: {
+    width: "80%",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  buttonContainer: {
+    width: "60%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 25,
+  },
+  button: {
+    width: "100%",
+    backgroundColor: "#267777",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 15,
+  },
+  buttonOutline: {
+    backgroundColor: "white",
+    marginTop: 5,
+    borderColor: "#267777",
+    borderWidth: 2,
+  },
+  buttonOutlineText: {
+    color: "#267777",
+    fontWeight: "700",
+    fontSize: 15,
+  },
+})
