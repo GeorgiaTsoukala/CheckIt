60f4fc97615054447a1a9c4d9bece0add83ee2a8
GeorgiaTsoukala <georgiats1999@gmail.com>
2024-04-27 14:54:43 +0200

    new user bug fix in vis

--- screens/VisualizationsScreen.js
+++ screens/VisualizationsScreen.js
@@ -12,8 +12,9 @@ const VisualizationsScreen = () => {
   const [viewModeBar, setViewModeBar] = useState('days') // For Bar plot, initially set to 'days'
   const [goals, setGoals] = useState([])
   const [data, setData] = useState([])
+  const [newUser, setNewUser] = useState(false) // For new users that dont'd have daily data yet
   const [loading, setLoading] = useState(true)
-  const [pieData, setPieData] = useState([]) //Data for pie plot
+  const [pieData, setPieData] = useState([]) // Data for pie plot
   const [scatterData, setScatterData] = useState([]) //Data for scatter plot
   const [barData, setBarData] = useState([])
   const [barDailyData, setBarDailyData] = useState([]) //Daily Data for bar plot
@@ -36,14 +37,19 @@ const VisualizationsScreen = () => {
         // fetch dailydata
         const querySnapshot = await getDocs(collection(datab, "users", auth.currentUser.uid, "dailydata"));
 
-        const allData = []
+        if (querySnapshot.empty){
+          setNewUser(true)
+        } else {
+          setNewUser(false)
+          const allData = []
 
-        querySnapshot.forEach(async (dataDoc) => {
-          const mydata = dataDoc.data();
-          allData.push(mydata)
-        });
+          querySnapshot.forEach(async (dataDoc) => {
+            const mydata = dataDoc.data();
+            allData.push(mydata)
+          });
 
-        setData(allData);
+          setData(allData);
+        }        
         
       } catch (error) {
         console.error('Error fetching the selected goals and data:', error);
@@ -270,178 +276,186 @@ const VisualizationsScreen = () => {
       </View>
 
       {/* diagrams */}
-      <ScrollView>
-        {/* first diagram */}
-        <View>          
-          <View style={globalStyles.center}>  
-            <Text style={styles.plotTitle}>{viewModeBar === 'days' ? "Daily Accomplished Goals Percentage" : "Monthly Accomplished Goals Percentage"}</Text>
-          </View>
-          {loading ?
-            <View style={{flex: 1, justifyContent:'center'}}>
-              <ActivityIndicator size="large" color="#63086B" />
+      {newUser ? 
+         <View style = {globalStyles.center}>
+         <Text style = {[globalStyles.subtitle, {width: '90%', marginTop: 60}]}>You haven't checked any goals yet!</Text>
+       </View>
+       : (
+        <ScrollView>
+          {/* first diagram */}
+          <View>          
+            <View style={globalStyles.center}>  
+              <Text style={styles.plotTitle}>{viewModeBar === 'days' ? "Daily Accomplished Goals Percentage" : "Monthly Accomplished Goals Percentage"}</Text>
             </View>
-          : (
-            <View style={styles.container}>
-              <VictoryChart
-                // domainPadding will add space to each side of VictoryBar to
-                // prevent it from overlapping the axis
-                domainPadding={15}
-                theme={VictoryTheme.material}
-                padding={{ top: 10, bottom: 40, left: 50, right: 40 }} 
-                height={300}
-              >                
-                <VictoryAxis
-                  // tickValues specifies both the number of ticks and where
-                  // they are placed on the axis
-                  tickValues={viewModeBar === 'days' ? [7, 6, 5, 4, 3, 2, 1] : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]} 
-                  tickFormat={viewModeBar === 'days' ? ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"] : ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]}
+            {loading ?
+              <View style={{flex: 1, justifyContent:'center'}}>
+                <ActivityIndicator size="large" color="#63086B" />
+              </View>
+            : (
+              <View style={styles.container}>
+                <VictoryChart
+                  // domainPadding will add space to each side of VictoryBar to
+                  // prevent it from overlapping the axis
+                  domainPadding={15}
+                  theme={VictoryTheme.material}
+                  padding={{ top: 10, bottom: 40, left: 50, right: 40 }} 
+                  height={300}
+                >                
+                  <VictoryAxis
+                    // tickValues specifies both the number of ticks and where
+                    // they are placed on the axis
+                    tickValues={viewModeBar === 'days' ? [7, 6, 5, 4, 3, 2, 1] : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]} 
+                    tickFormat={viewModeBar === 'days' ? ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"] : ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]}
+                    style={{
+                      tickLabels: { fontSize: 12, fill: colors.grey600 },
+                      // axis: { stroke: "transparent" }, // Hide the axis line
+                    }}
+                  />
+                  <VictoryAxis
+                    dependentAxis
+                    // tickFormat specifies how ticks should be displayed
+                    tickFormat={(x) => (`${x}%`)}
+                    style={{
+                      tickLabels: { fontSize: 12, fill: colors.grey600 },
+                      // axis: { stroke: "transparent" }, // Hide the axis line
+                    }}
+                  />
+                  <VictoryBar
+                    data={barData}
+                    x="tag"
+                    y="value"
+                    barWidth={16}
+                    style={{
+                      data: { fill: '#A2B1F7' },
+                    }}
+                  />
+                </VictoryChart>
+                <View style={{ alignItems: 'center', marginBottom:20 }}>
+                  <SegmentedButtons
+                    theme={{colors: {secondaryContainer:'#A2B1F7'}}}
+                    style={{ width: 200}}
+                    onValueChange={value => setViewModeBar(value)} 
+                    value={viewModeBar}
+                    buttons={[
+                      {value: 'days', label: 'Days'},
+                      {value: 'months', label: 'Months'}
+                    ]}  
+                  />
+                </View>
+              </View>            
+            )}
+          </View>
+
+          <Divider />
+
+          {/* second diagram */}
+          <View>
+            <View style={globalStyles.center}>  
+              <Text style={styles.plotTitle}>Relation of Emotions and Accomplished Goals</Text>
+            </View>
+            {loading ?
+              <View style={{flex: 1, justifyContent:'center'}}>
+                <ActivityIndicator size="large" color="#63086B" />
+              </View>
+            : (
+              <View style={styles.container}>
+                <VictoryChart
+                  // domainPadding will add space to each side of VictoryBar to
+                  // prevent it from overlapping the axis
+                  domainPadding={15}
+                  theme={VictoryTheme.material}
+                  padding={{ top: 0, bottom: 50, left: 50, right: 40 }} 
+
+                >                
+                  <VictoryAxis
+                    // tickValues specifies both the number of ticks and where
+                    // they are placed on the axis
+                    tickValues={[1, 2, 3, 4, 5]} 
+                    tickFormat={["very sad", "sad", "neutral", "happy", "very happy"]}
+                    style={{
+                      tickLabels: { fontSize: 12, fill: colors.grey600 },
+                      // axis: { stroke: "transparent" }, // Hide the axis line
+                    }}
+                  />
+                  <VictoryAxis
+                    dependentAxis
+                    // tickFormat specifies how ticks should be displayed
+                    tickFormat={(x) => {
+                      if (x % 1 !== 0) {
+                        return `${x.toFixed(2)}%`; // Display with two decimal points
+                      } else {
+                        return `${x}%`; // Display without any decimal points
+                      }
+                    }} //(`${x}%`)
+                    style={{
+                      tickLabels: { fontSize: 12, fill: colors.grey600 },
+                      // axis: { stroke: "transparent" }, // Hide the axis line
+                    }}
+                  />
+                  <VictoryScatter
+                    style={{ data: { fill: "#c43a81" }, labels: { fill: "white", fontSize: 10}}}
+                    bubbleProperty="amount"
+                    maxBubbleSize={15}
+                    minBubbleSize={5}
+                    data={scatterData}
+                    labels={({ datum }) => datum.amount}
+                    labelComponent={<VictoryLabel dy={5}/>}
+                    x="emotion"
+                    y="value"
+                  />
+                </VictoryChart>
+              </View>
+            )}
+          </View>
+
+          <Divider />
+
+          {/* third diagram */}
+          <View>
+            <View style={globalStyles.center}>  
+              <Text style={styles.plotTitle}>Pie title</Text>
+            </View>
+          {loading ?
+              <View style={{flex: 1, justifyContent:'center'}}>
+                <ActivityIndicator size="large" color="#63086B" />
+              </View>
+            : (
+              <View style={{flex: 1, alignItems: 'center', justifyContent:'center'}}>
+                <VictoryPie
+                  // padAngle={({ datum }) => datum.y}
+                  colorScale={pieColorScale}
+                  data={pieData}
+                  labels={() => ''}
+                  padding={{top:10, left:20, right:20, bottom:10}}
                   style={{
-                    tickLabels: { fontSize: 12, fill: colors.grey600 },
-                    // axis: { stroke: "transparent" }, // Hide the axis line
+                    data: {
+                      stroke: colors.grey50, strokeWidth: 3
+                    }
                   }}
+                  // Other props for your VictoryPie component
                 />
-                <VictoryAxis
-                  dependentAxis
-                  // tickFormat specifies how ticks should be displayed
-                  tickFormat={(x) => (`${x}%`)}
+                <VictoryLegend
+                  orientation="horizontal" // Display the legend horizontally
+                  itemsPerRow={3}
+                  gutter={20} // Add spacing between legend items
+                  height={190}
                   style={{
-                    tickLabels: { fontSize: 12, fill: colors.grey600 },
-                    // axis: { stroke: "transparent" }, // Hide the axis line
+                    labels: {
+                      fill: colors.grey600
+                    }
                   }}
-                />
-                <VictoryBar
-                  data={barData}
-                  x="tag"
-                  y="value"
-                  barWidth={16}
-                  style={{
-                    data: { fill: '#A2B1F7' },
-                  }}
-                />
-              </VictoryChart>
-              <View style={{ alignItems: 'center', marginBottom:20 }}>
-                <SegmentedButtons
-                  theme={{colors: {secondaryContainer:'#A2B1F7'}}}
-                  style={{ width: 200}}
-                  onValueChange={value => setViewModeBar(value)} 
-                  value={viewModeBar}
-                  buttons={[
-                    {value: 'days', label: 'Days'},
-                    {value: 'months', label: 'Months'}
-                  ]}  
+                  data={pieData.map(({ x }, index) => ({
+                    name: x,
+                    symbol: { fill: pieColorScale[index % pieColorScale.length] }, // Use color from color scale
+                  }))}
                 />
               </View>
-            </View>            
-          )}
-        </View>
-
-        <Divider />
-
-        {/* second diagram */}
-        <View>
-          <View style={globalStyles.center}>  
-            <Text style={styles.plotTitle}>Relation of Emotions and Accomplished Goals</Text>
-          </View>
-          {loading ?
-            <View style={{flex: 1, justifyContent:'center'}}>
-              <ActivityIndicator size="large" color="#63086B" />
-            </View>
-          : (
-            <View style={styles.container}>
-              <VictoryChart
-                // domainPadding will add space to each side of VictoryBar to
-                // prevent it from overlapping the axis
-                domainPadding={15}
-                theme={VictoryTheme.material}
-                padding={{ top: 0, bottom: 50, left: 50, right: 40 }} 
-
-              >                
-                <VictoryAxis
-                  // tickValues specifies both the number of ticks and where
-                  // they are placed on the axis
-                  tickValues={[1, 2, 3, 4, 5]} 
-                  tickFormat={["very sad", "sad", "neutral", "happy", "very happy"]}
-                  style={{
-                    tickLabels: { fontSize: 12, fill: colors.grey600 },
-                    // axis: { stroke: "transparent" }, // Hide the axis line
-                  }}
-                />
-                <VictoryAxis
-                  dependentAxis
-                  // tickFormat specifies how ticks should be displayed
-                  tickFormat={(x) => {
-                    if (x % 1 !== 0) {
-                      return `${x.toFixed(2)}%`; // Display with two decimal points
-                    } else {
-                      return `${x}%`; // Display without any decimal points
-                    }
-                  }} //(`${x}%`)
-                  style={{
-                    tickLabels: { fontSize: 12, fill: colors.grey600 },
-                    // axis: { stroke: "transparent" }, // Hide the axis line
-                  }}
-                />
-                <VictoryScatter
-                  style={{ data: { fill: "#c43a81" }, labels: { fill: "white", fontSize: 10}}}
-                  bubbleProperty="amount"
-                  maxBubbleSize={15}
-                  minBubbleSize={5}
-                  data={scatterData}
-                  labels={({ datum }) => datum.amount}
-                  labelComponent={<VictoryLabel dy={5}/>}
-                  x="emotion"
-                  y="value"
-                />
-              </VictoryChart>
-            </View>
-          )}
-        </View>
-
-        <Divider />
-
-        {/* third diagram */}
-        <View>
-          <View style={globalStyles.center}>  
-            <Text style={styles.plotTitle}>Pie title</Text>
-          </View>
-         {loading ?
-            <View style={{flex: 1, justifyContent:'center'}}>
-              <ActivityIndicator size="large" color="#63086B" />
-            </View>
-          : (
-            <View style={{flex: 1, alignItems: 'center', justifyContent:'center'}}>
-              <VictoryPie
-                // padAngle={({ datum }) => datum.y}
-                colorScale={pieColorScale}
-                data={pieData}
-                labels={() => ''}
-                padding={{top:10, left:20, right:20, bottom:10}}
-                style={{
-                  data: {
-                    stroke: colors.grey50, strokeWidth: 3
-                  }
-                }}
-                // Other props for your VictoryPie component
-              />
-              <VictoryLegend
-                orientation="horizontal" // Display the legend horizontally
-                itemsPerRow={3}
-                gutter={20} // Add spacing between legend items
-                height={190}
-                style={{
-                  labels: {
-                    fill: colors.grey600
-                  }
-                }}
-                data={pieData.map(({ x }, index) => ({
-                  name: x,
-                  symbol: { fill: pieColorScale[index % pieColorScale.length] }, // Use color from color scale
-                }))}
-              />
-            </View>
-          )}
-        </View>              
-      </ScrollView>
+            )}
+          </View>              
+        </ScrollView>
+       )
+      }
+      
     </View>
   )
 }
